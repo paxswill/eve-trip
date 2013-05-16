c5a4866fde7789d5d780c6fbff4d130455d12c4a
Will Ross <paxswill@gmail.com>
2013-05-16 11:50:18 -0400

    Factor the triangle inequality test out
    Preparation for Leveshtein testing.

--- jbmap/test_bktree.py
+++ jbmap/test_bktree.py
@@ -1,28 +1,9 @@
 import unittest
 from jbmap import bktree
 
-class TestEuclidian(unittest.TestCase):
+class TestMetric(unittest.TestCase):
 
-    def test_different_lengths(self):
-        with self.assertRaises(AssertionError):
-            bktree.euclidian_distance((0, 0), (4, 4, 4))
-
-    def test_one_dimensions(self):
-        result = bktree.euclidian_distance((0,), (5,))
-        self.assertAlmostEqual(result, 5.0)
-
-    def test_two_dimensions(self):
-        result = bktree.euclidian_distance((0, 0), (4, 4))
-        self.assertAlmostEqual(result, 5.656854249)
-
-    def test_three_dimensions(self):
-        result = bktree.euclidian_distance((0, 0, 0), (-2, -2, -4))
-        self.assertAlmostEqual(result, 4.898979486)
-
-    def test_triangle_inequality(self):
-        a = (1, 2, 3)
-        b = (4, 5, 6)
-        c = (7, 8, 9)
+    def assertTriangleInequality(self, a, b, c):
         # Identity
         self.assertAlmostEqual(bktree.euclidian_distance(a, a), 0.0)
         self.assertAlmostEqual(bktree.euclidian_distance(b, b), 0.0)
@@ -44,6 +25,31 @@ class TestEuclidian(unittest.TestCase):
         self.assertGreaterEqual(bktree.euclidian_distance(a, c) +
                                 bktree.euclidian_distance(a, b),
                                 bktree.euclidian_distance(c, b))
+
+
+class TestEuclidian(TestMetric):
+
+    def test_different_lengths(self):
+        with self.assertRaises(AssertionError):
+            bktree.euclidian_distance((0, 0), (4, 4, 4))
+
+    def test_one_dimensions(self):
+        result = bktree.euclidian_distance((0,), (5,))
+        self.assertAlmostEqual(result, 5.0)
+
+    def test_two_dimensions(self):
+        result = bktree.euclidian_distance((0, 0), (4, 4))
+        self.assertAlmostEqual(result, 5.656854249)
+
+    def test_three_dimensions(self):
+        result = bktree.euclidian_distance((0, 0, 0), (-2, -2, -4))
+        self.assertAlmostEqual(result, 4.898979486)
+
+    def test_triangle_inequality(self):
+        a = (1, 2, 3)
+        b = (4, 5, 6)
+        c = (7, 8, 9)
+        self.assertTriangleInequality(a, b, c)
 
 
 if __name__ == '__main__':
